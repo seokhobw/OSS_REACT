7e997e343272704d94c6803beb81a341d748dac1
Seokho Lee <joseph000617@gmail.com>
2025-11-15 23:21:47 +0900

    내 첫번째 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// App.js
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    alert("버튼이 클릭되었습니다!");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="main-title">Welcome to My First React Project!</h1>
+
+      <p className="description">
+        This is a 이석호's customized React page. 아래에서 변경된 스타일을 확인해보세요!
+      </p>
+
+      <button className="custom-button" onClick={handleClick}>
+        클릭해보세요
+      </button>
     </div>
   );
 }
